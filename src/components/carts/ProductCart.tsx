17a7d5e133219cb21b59ca2040c234e48800e308
test <esfahani.aliyahoo@gmail.com>
2024-07-20 15:55:33 +0330

    refcator: productCard ui

--- src/components/carts/ProductCart.tsx
+++ src/components/carts/ProductCart.tsx
@@ -1,4 +1,15 @@
-import { Box, Button, Rating, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Rating,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
@@ -13,6 +24,7 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Link } from "react-router-dom";
 
 interface ProductCartProps {
   data: IProduct;
@@ -20,7 +32,7 @@ interface ProductCartProps {
 
 const ProductCart = ({ data }: ProductCartProps) => {
   // product info
-  const { title, image, rating, price, category } = data;
+  const { title, image, rating, price, category, id } = data;
 
   // current product data
   const { carts } = useSelector((state: RootState) => state.carts);
@@ -48,60 +60,65 @@ const ProductCart = ({ data }: ProductCartProps) => {
   };
 
   return (
-    <Box
-      component="div"
+    <Card
       sx={(theme) => ({
         backgroundColor:
           theme.palette.mode === "dark"
             ? theme.palette.secondary.dark
             : theme.palette.secondary.light,
       })}
-      className="col-span-12 md:col-span-6 xl:col-span-4 2xl:col-span-3 rounded-md p-3"
+      className="col-span-12 md:col-span-6 xl:col-span-4  2xl:col-span-3 rounded-md p-3"
     >
-      <Box
-        component="div"
-        className="overflow-hidden relative bg-yellow w-full h-[300px]"
-      >
-        <LazyLoadImage
-          alt={title}
-          src={image}
-          effect="blur"
-          className="!w-full !h-full !object-fit !object-center"
-          wrapperClassName="w-full h-full object-fit object-center"
-          threshold={5}
-          useIntersectionObserver
-        />
-        <Typography
-          sx={(theme) => ({
-            backgroundColor:
-              theme.palette.mode === "dark"
-                ? theme.palette.secondary.dark
-                : theme.palette.secondary.light,
-          })}
-          variant="caption"
-          className="absolute px-2 py-1 top-2 rounded-md min-w-[50px] flex items-center justify-center z-50 left-2"
-        >
-          {category}
-        </Typography>
-      </Box>
-      <Box component="div" className="p-2 flex flex-col">
-        <Tooltip title={title} placement="top">
-          <Typography variant="body1" className="truncate">
-            {title}
-          </Typography>
-        </Tooltip>
-        <Box component="div" className="flex mt-2 items-center justify-between">
-          <Rating value={rating.rate} readOnly precision={0.5} />
-          <Typography>{rating.count}</Typography>
-        </Box>
-        <Box component="div" className="flex mt-2 items-center gap-x-2">
-          <LocalOfferIcon />
-          <Typography variant="h6">{price.toLocaleString("en")}</Typography>
-        </Box>
+      <Link to={`product/${id}`}>
+        <CardActionArea>
+          <CardMedia className="overflow-hidden relative bg-yellow w-full h-[300px]">
+            <LazyLoadImage
+              alt={title}
+              src={image}
+              effect="blur"
+              className="!w-full !h-full !object-fit !object-center"
+              wrapperClassName="w-full h-full object-fit object-center"
+              threshold={5}
+              useIntersectionObserver
+            />
+            <Typography
+              sx={(theme) => ({
+                backgroundColor:
+                  theme.palette.mode === "dark"
+                    ? theme.palette.secondary.dark
+                    : theme.palette.secondary.light,
+              })}
+              variant="caption"
+              className="absolute px-2 py-1 top-2 rounded-md min-w-[50px] flex items-center justify-center z-50 left-2"
+            >
+              {category}
+            </Typography>
+          </CardMedia>
+          <CardContent component="div" className="p-2 flex flex-col">
+            <Tooltip title={title} placement="top">
+              <Typography variant="body1" className="truncate">
+                {title}
+              </Typography>
+            </Tooltip>
+            <Box
+              component="div"
+              className="flex mt-2 items-center justify-between"
+            >
+              <Rating value={rating.rate} readOnly precision={0.5} />
+              <Typography>{rating.count}</Typography>
+            </Box>
+            <Box component="div" className="flex mt-2 items-center gap-x-2">
+              <LocalOfferIcon />
+              <Typography variant="h6">{price.toLocaleString("en")}</Typography>
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+      <CardActions className="w-full">
         {currentProduct ? (
           <Box
             component="div"
-            className="flex mt-4 items-center justify-between"
+            className="flex mt-4 items-center w-full justify-between"
           >
             {currentProduct.quantity > 1 ? (
               <Button
@@ -130,14 +147,14 @@ const ProductCart = ({ data }: ProductCartProps) => {
           <Button
             onClick={() => handleProductAction("ADD")}
             color="primary"
-            className="mt-4"
+            className="mt-4 w-full"
             variant="contained"
           >
             Add To Cart
           </Button>
         )}
-      </Box>
-    </Box>
+      </CardActions>
+    </Card>
   );
 };
 
