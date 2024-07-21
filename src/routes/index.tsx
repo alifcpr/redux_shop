ee452939eb0b163a900d1883409e5d5b7b3d6e19
test <esfahani.aliyahoo@gmail.com>
2024-07-21 13:05:34 +0330

    refactor: import pages with lazy func and create a pageLoading component for suspense fallback

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,74 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import HomePage from "../pages/home/HomePage";
-import ShoppingCartPage from "../pages/shopping-cart/ShoppingCartPage";
-import PaymentSuccessfullyPage from "../pages/payment-successfully/PaymentSuccessfullyPage";
-import ProductDetailPage from "../pages/product/id/ProductDetailPage";
+import { Suspense, lazy } from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const HomePage = lazy(() => import("../pages/home/HomePage"));
+
+const ShoppingCartPage = lazy(
+  () => import("../pages/shopping-cart/ShoppingCartPage")
+);
+const ProductDetailPage = lazy(
+  () => import("../pages/payment-successfully/PaymentSuccessfullyPage")
+);
+const PaymentSuccessfullyPage = lazy(
+  () => import("../pages/product/id/ProductDetailPage")
+);
+
+const PageLoading = () => {
+  return (
+    <Box
+      component="div"
+      sx={(theme) => ({
+        backgroundColor:
+          theme.palette.mode === "dark"
+            ? theme.palette.secondary.main
+            : theme.palette.primary.main,
+      })}
+      className="min-w-full flex opacity-70 items-center justify-center min-h-screen absolute top-0 left-0 z-[9999]"
+    >
+      <CircularProgress color="warning" />
+    </Box>
+  );
+};
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/shopping-cart", element: <ShoppingCartPage /> },
-      { path: "/product/:id", element: <ProductDetailPage /> },
-      { path: "/payment/successfully", element: <PaymentSuccessfullyPage /> },
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<PageLoading />}>
+            <HomePage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/shopping-cart",
+        element: (
+          <Suspense fallback={<PageLoading />}>
+            <ShoppingCartPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/product/:id",
+        element: (
+          <Suspense fallback={<PageLoading />}>
+            <ProductDetailPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/payment/successfully",
+        element: (
+          <Suspense fallback={<PageLoading />}>
+            <PaymentSuccessfullyPage />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
